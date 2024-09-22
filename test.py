862ab341830bd85bea7aa20cad798e25fa897fa5
MahadAc <mahadach786@gmail.com>
2024-09-21 18:24:34 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st 
 from sklearn import preprocessing
 import pickle
-   st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible") 
+st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible") 
     
    
 git add test.py
