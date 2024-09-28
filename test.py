55246b4f19e3a12354223ba396181168f4b5a692
MahadAc <mahadach786@gmail.com>
2024-09-28 15:25:58 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import streamlit as st 
 import pickle
 st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible") 
     
