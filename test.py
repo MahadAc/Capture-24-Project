847213a86a315d901bf5ed809c895eb2e28af745
MahadAc <mahadach786@gmail.com>
2024-09-28 09:25:16 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,4 @@ from sklearn import preprocessing
 import pickle
 st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible") 
     
-   
-git add test.py
-git commit -m "First update"
-git push
+
