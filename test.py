ecc7fbb245f57f76c04be97a113b949bc365746c
MahadAc <mahadach786@gmail.com>
2024-09-29 08:16:12 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import numpy as np
 import pandas as pd
 import pickle
 import streamlit as st
-st.file_uploader(label=None, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible") 
+st.file_uploader("acceleremoter data", type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible") 
     
 
