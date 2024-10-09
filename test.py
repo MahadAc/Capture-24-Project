8694c087f9ed017817e386732ce11449b359e2f6
MahadAc <mahadach786@gmail.com>
2024-10-08 18:17:24 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import pickle
 import streamlit as st
+import joblib
+import features
 st.file_uploader("accelerometer data", type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible") 
     
 
