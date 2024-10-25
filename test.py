0937c12bc1c286ce384c6aec78641cd00fafaf43
MahadAc <mahadach786@gmail.com>
2024-10-24 18:34:04 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,13 +4,8 @@ import pickle
 import streamlit as st
 import joblib
 import features
-st.file_uploader("accelerometer data", type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible") 
-
 import joblib
-joblib.dump(clf4, "/content/drive/MyDrive/Veritas - Mahad/random_forrest_model.joblib")
-import os
-joblib.dump(clf4,"/content/drive/MyDrive/Veritas - Mahad/random_forrest_model.joblib", compress=3)
-print(f"Compressed Random Forest: {np.round(os.path.getsize('/content/drive/MyDrive/Veritas - Mahad/random_forrest_model.joblib') / 1024 / 1024, 2) } MB")
+st.file_uploader("accelerometer data", type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible") 
 loaded_othermodel = joblib.load("/content/drive/MyDrive/Veritas - Mahad/random_forrest_model.joblib")
 model = lambda x: 42
 
@@ -27,7 +22,7 @@ def main():
     """
     st.markdown(html_temp, unsafe_allow_html = True)
 
- if st.button("Predict"): 
+    if st.button("Predict"): 
  
 # In Progress From Here
 # 2. Here is where you need to add code to convert the raw xyz data into features
@@ -36,9 +31,9 @@ features = []
 features.extract_features
 
 # 4. This is where the model actually makes its predictions
-        # You will need to change this to model.predict, once you've setup the 
-        prediction = model(features)
-        model.predict 
+# You will need to change this to model.predict, once you've setup the 
+prediction = model(features)
+model.predict 
 # 5. Here is where we create an output to display to the user
         # Currently, it just returns some text. 
         # Instead, we want to create the figures showing participant activity over time (Similar to plot_compare() in the notebook)
@@ -55,4 +50,5 @@ if __name__=='__main__':
     main()
 # Found this for image in streamlit
 st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
 
