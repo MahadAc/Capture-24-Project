f9fbeccf43f7b4bcd51f54ebaa72e9e21f72d8db
MahadAc <mahadach786@gmail.com>
2024-10-26 08:15:53 -0700

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,21 @@ st.file_uploader("accelerometer data", type=None, accept_multiple_files=False, k
 loaded_othermodel = joblib.load("/content/drive/MyDrive/Veritas - Mahad/random_forrest_model.joblib")
 model = lambda x: 42
 
-
+# Create a function to extract data for any participant
+def extract_data(participant_number):
+    #Load another participant data
+    df = pd.read_csv(data_directory + f"/P{participant_number:03d}.csv.gz",
+    index_col='time', parse_dates=['time'],
+    dtype={'x': 'f4', 'y': 'f4', 'z': 'f4', 'annotation': 'string'})
+    # Simplify annotations
+    df['label'] = (anno_label_dict['label:Willetts2018']
+    .reindex(df['annotation'])
+    .to_numpy())
+    # Extract windows
+    X_values, Y_values = extract_windows(df)
+    # Extract features
+    X_features = pd.DataFrame([features.extract_features(x) for x in X_values])
+    return X_features, Y_values
 
 
 
@@ -23,30 +37,14 @@ def main():
     st.markdown(html_temp, unsafe_allow_html = True)
 
     if st.button("Predict"): 
- 
-# Create a function to extract data for any participant
-def extract_data(participant_number):
-  # Load another participant data
-  df = pd.read_csv(data_directory + f"/P{participant_number:03d}.csv.gz",
-  index_col='time', parse_dates=['time'],
-  dtype={'x': 'f4', 'y': 'f4', 'z': 'f4', 'annotation': 'string'})
-  # Simplify annotations
-  df['label'] = (anno_label_dict['label:Willetts2018']
-  .reindex(df['annotation'])
-  .to_numpy())
-  # Extract windows
-  X_values, Y_values = extract_windows(df)
-  # Extract features
-  X_features = pd.DataFrame([features.extract_features(x) for x in X_values])
-  return X_features, Y_values
-features = []
-features.extract_features
-
-# 4. This is where the model actually makes its predictions
-# You will need to change this to model.predict, once you've setup the 
-prediction = model(features)
-model.predict 
-# 5. Here is where we create an output to display to the user
+        features = []
+        features.extract_features
+        
+        # 4. This is where the model actually makes its predictions
+        # You will need to change this to model.predict, once you've setup the 
+        prediction = model(features)
+        model.predict 
+        # 5. Here is where we create an output to display to the user
         # Currently, it just returns some text. 
         # Instead, we want to create the figures showing participant activity over time (Similar to plot_compare() in the notebook)
         # I don't actually know how to display images in streamlit yet, you will need to look this up
@@ -55,8 +53,10 @@ model.predict
             text = ">50K"
         else:
             text = "<=50K"
-
+        
         st.success('Employee Income is {}'.format(text))
+
+
       
 if __name__=='__main__': 
     main()
