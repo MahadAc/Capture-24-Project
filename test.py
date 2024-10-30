00ac1387b9499c23a1f8c3f63229c81a18748403
Angus Nicolson <angusjnicolson@gmail.com>
2024-10-30 10:28:53 +0000

    Add loading of example data and figure plotting

--- test.py
+++ test.py
@@ -9,40 +9,83 @@ df_upload = st.file_uploader("accelerometer data", type=None, accept_multiple_fi
 model = joblib.load("random_forrest_model.joblib")
 
 #Create function to extract windows
-def extract_windows(data, winsize='10s'):
-        X, Y = [], []
-        for t, w in data.resample(winsize, origin='start'):
-            # Check window has no NaNs and is of correct length
-            # 10s @ 100Hz = 1000 ticks
-            if w.isna().any().any() or len(w) != 1000:
+def make_windows(data, winsec=10, sample_rate=100, dropna=True, verbose=False, label_col='label'):
+    X, Y, T = [], [], []
+
+    if label_col not in data.cols:
+        return_y = False
+    else:
+        return_y = True
+
+    for t, w in tqdm(data.resample(f"{winsec}s", origin='start'), disable=not verbose):
+
+        if len(w) < 1:
+            continue
+
+        t = t.to_numpy()
+
+        x = w[['x', 'y', 'z']].to_numpy()
+
+        if return_y:
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", message="Unable to sort modes")
+                y = w[label_col].mode(dropna=False).iloc[0]
+    
+            if dropna and pd.isna(y):  # skip if annotation is NA
                 continue
 
-            x = w[['x', 'y', 'z']].to_numpy()
-            y = w['label'].mode(dropna=False).item()
+        if not is_good_window(x, sample_rate, winsec):  # skip if bad window
+            continue
 
-            X.append(x)
+        X.append(x)
+        if return_y:
             Y.append(y)
+        T.append(t)
 
-        X = np.stack(X)
+    X = np.stack(X)
+    T = np.stack(T)
+    if return_y:
         Y = np.stack(Y)
-        return X, Y
+    else:
+        Y = None
+    return X, Y, T
 
+def is_good_window(x, sample_rate, winsec):
+    ''' Check there are no NaNs and len is good '''
+
+    # Check window length is correct
+    window_len = sample_rate * winsec
+    if len(x) != window_len:
+        return False
+
+    # Check no nans
+    if np.isnan(x).any():
+        return False
+
+    return True
+            
 # Create a function to extract data for any participant
 def extract_data(df):
     #Load another participant data
-    #df = pd.read_csv(f"/P{participant_number:03d}.csv.gz",
-    #index_col='time', parse_dates=['time'],
-    #dtype={'x': 'f4', 'y': 'f4', 'z': 'f4', 'annotation': 'string'})
-    #Simplify annotations
-    anno_label_dict = pd.read_csv('annotation-label-dictionary.csv', index_col = 'annotation', dtype = 'string')
-    df['label'] = (anno_label_dict['label:Willetts2018']
-    .reindex(df['annotation'])
-    .to_numpy())
+    if df is None:
+        df = pd.read_csv(
+            "./example_data_P142.csv.gz",
+            index_col='time', parse_dates=['time'],
+            dtype={'x': 'f4', 'y': 'f4', 'z': 'f4', 'annotation': 'string'}
+        )
+        #Simplify annotations
+        anno_label_dict = pd.read_csv('annotation-label-dictionary.csv', index_col = 'annotation', dtype = 'string')
+        df['label'] = (anno_label_dict['label:Willetts2018'].reindex(df['annotation']).to_numpy())
     # Extract windows
-    X_values, Y_values = extract_windows(df)
+    X, Y, T = make_windows(df, winsec=10, label_col="label")
     # Extract features
-    X_features = pd.DataFrame([features.extract_features(x) for x in X_values])
-    return X_features, Y_values
+    X_features = pd.DataFrame([features.extract_features(x) for x in X])
+    return X_features, Y, T
+
+def plot_figure(prediction, T, Y=None):
+    fig, ax = pt.subplots()
+    ax.plot(np.arange(10), np.arange(10)**2)
+    return fig, ax
 
 def main(): 
     st.title("Accelerometer Data")
@@ -56,13 +99,11 @@ def main():
     if st.button("Predict"): 
         # 4. This is where the model actually makes its predictions
         # You will need to change this to model.predict, once you've setup the 
-        X_features, Y_values = extract_data(df_upload)
+        X_features, Y, T = extract_data(df_upload)
         prediction = model.predict(X_features) 
-        # 5. Here is where we create an output to display to the user
-        # Currently, it just returns some text. 
-        # Instead, we want to create the figures showing participant activity over time (Similar to plot_compare() in the notebook)
-        # I don't actually know how to display images in streamlit yet, you will need to look this up
-        st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+        fig = plot_figure(prediction, T, Y)
+        st.pyplot(fig=fig)
 
 
       
